0d1c6894189b785758583f96044a4570205b5a5a
Christopher Hugh Kelly <christopher.kelly1997@gmail.com>
2023-08-30 15:12:27 -0600

    adds the prop functions in app and takes them in as parameters and gets the data and adopts pets and changes the filter type

--- src/components/App.js
+++ src/components/App.js
@@ -7,6 +7,49 @@ function App() {
   const [pets, setPets] = useState([]);
   const [filters, setFilters] = useState({ type: "all" });
 
+  function onChangeType(nwtype)
+  {
+    const mynwfilters = {...filters};
+    mynwfilters.type = nwtype;
+    setFilters(mynwfilters);
+  }
+
+  function onFindPetsClick()
+  {
+    const baseurl = "http://localhost:3001/pets";
+    let myqry = "";
+    if (filters.type === "all");
+    else if (filters.type === "cat" || filters.type === "dog" || filters.type === "micropig")
+    {
+      myqry = "?type=" + filters.type;
+    }
+    //else;//do nothing
+    console.log("myurl = baseurl + myqry = " + (baseurl + myqry));
+    fetch(baseurl + myqry).then(r => r.json()).then((response) => {
+      console.log("response = ", response);
+      setPets(response);
+    }).catch((err) => {
+      console.error("there was a problem getting the data from the server!");
+      console.error(err);
+      alert("failed to get the list of pets from the server! See log for details!");
+    });
+  }
+
+  function onAdoptPet(mid)
+  {
+    //adopt the pet and update the state here
+    let nwpets = pets.map((pet, index) => {
+      if (pet.id == mid)
+      {
+        let mynwpet = {...pet};
+        mynwpet.isAdopted = true;
+        return mynwpet;
+      }
+      else return pet;
+    });
+    setPets(nwpets);
+  }
+
   return (
     <div className="ui container">
       <header>
@@ -15,10 +58,10 @@ function App() {
       <div className="ui container">
         <div className="ui grid">
           <div className="four wide column">
-            <Filters />
+            <Filters onChangeType={onChangeType} onFindPetsClick={onFindPetsClick} />
           </div>
           <div className="twelve wide column">
-            <PetBrowser />
+            <PetBrowser pets={pets} onAdoptPet={onAdoptPet} />
           </div>
         </div>
       </div>
--- src/components/PetBrowser.js
+++ src/components/PetBrowser.js
@@ -1,9 +1,14 @@
 import React from "react";
-
 import Pet from "./Pet";
 
-function PetBrowser() {
-  return <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>;
+function PetBrowser({pets, onAdoptPet}) {
+  const petobjs = pets.map((pet) => {
+    return null;
+  });
+
+  return (
+    <div className="ui cards">PET COMPONENT SHOULD GO HERE</div>
+  );
 }
 
 export default PetBrowser;
